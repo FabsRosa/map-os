2235a31366c5dd159d26d7abb614656cee76595c
Fabrício Rosa <fabsrosa0@gmail.com>
2024-08-09 11:15:10 -0400

    Code cleaner

--- src/MapComponent.jsx
+++ src/MapComponent.jsx
@@ -33,7 +33,7 @@ const getMarkerIcon = (isHighlighted, nometec, tec) => {
 };
 
 const InfoWindowContentOrder = ({ order, isEditing, onEditClick, onTecChange, tecList }) => {
-  const isTecInList = tecList? tecList.some(tec => tec.id == order.idTec) : false;
+  const isTecInList = tecList ? tecList.some(tec => tec.id == order.idTec) : false;
 
   return (
     <div style={{ backgroundColor: '#fff', color: '#000', padding: '5px', borderRadius: '5px' }}>
@@ -44,7 +44,7 @@ const InfoWindowContentOrder = ({ order, isEditing, onEditClick, onTecChange, te
         • Técnico:&nbsp;
         {isEditing ? (
           <select className='custom-select' value={isTecInList ? order.idTec : ''} onChange={(e) => onTecChange(e.target.value)}>
-            <option value="" disabled>TERCEIRIZADO</option>
+            <option value='' disabled>TERCEIRIZADO</option>
             {tecList.map(tec => (
               <option key={tec.id} value={tec.id}>
                 {tec.nome}
@@ -83,19 +83,17 @@ const fetchOrdersData = async () => {
     const responseOS = await apiClient.get('/maps/OS');
 
     if (responseOS.ok && responseOS.data) {
-      const ordersData = responseOS.data.map(order => {
-        return {
-          id: order.idOS.toString(),
-          lat: parseFloat(order.lat),
-          lng: parseFloat(order.lng),
-          clientID: order.idCliente,
-          clientName: order.nomeCliente,
-          def: order.defeito,
-          desc: order.descricao,
-          idTec: order.idTec,
-          nomeTec: order.nomeTec,
-        };
-      });
+      const ordersData = responseOS.data.map(order => ({
+        id: order.idOS.toString(),
+        lat: parseFloat(order.lat),
+        lng: parseFloat(order.lng),
+        clientID: order.idCliente,
+        clientName: order.nomeCliente,
+        def: order.defeito,
+        desc: order.descricao,
+        idTec: order.idTec,
+        nomeTec: order.nomeTec,
+      }));
 
       return ordersData;
     } else {
@@ -112,13 +110,11 @@ const fetchMotosData = async () => {
     const responseMoto = await apiClient.get('/maps/vehicleLocation');
 
     if (responseMoto.ok && responseMoto.data) {
-      const motosData = responseMoto.data.map(moto => {
-        return {
-          id: moto.placa,
-          lat:parseFloat(moto.lat),
-          lng:parseFloat(moto.lng),
-        };
-      });
+      const motosData = responseMoto.data.map(moto => ({
+        id: moto.placa,
+        lat: parseFloat(moto.lat),
+        lng: parseFloat(moto.lng),
+      }));
 
       return motosData;
     } else {
@@ -143,14 +139,14 @@ const fetchTechnicians = async () => {
 
         return {
           id: tec.idTec,
-          nome:tec.nomeTec,
+          nome: tec.nomeTec,
           color: pinColors[colorIndex++ % pinColors.length],
         };
       });
 
       return tecData;
     } else {
-      console.error('Error fetching orders:', responseMoto.problem);
+      console.error('Error fetching orders:', response.problem);
     }
   } catch (error) {
     console.error('Error fetching orders:', error);
@@ -184,18 +180,18 @@ const MapComponent = () => {
 
     fetchMapData(); // Initial fetch
 
-    const intervalId = setInterval(fetchMapData,5000); // Poll every 10 seconds
+    const intervalId = setInterval(fetchMapData, 5000); // Poll every 5 seconds
 
     return () => clearInterval(intervalId); // Cleanup on component unmount
   }, []);
 
   useEffect(() => {
     const fetchTecData = async () => {
-    const tecData = await fetchTechnicians();
-    setTecList(tecData);
-  };
+      const tecData = await fetchTechnicians();
+      setTecList(tecData);
+    };
 
-  fetchTecData();
+    fetchTecData();
   }, []);
 
   const handleMapClick = useCallback(() => {
@@ -219,7 +215,7 @@ const MapComponent = () => {
   const handleEditTech = (order) => {
     setEditingOrder(order.id);
   };
-  
+
   const handleTecChange = async (idNewTech) => {
     const nomeTec = tecList ? tecList.find(tec => tec.id == idNewTech).nome : '';
 
@@ -230,86 +226,86 @@ const MapComponent = () => {
 
     const updatedSelectedOrder = selectedOrder ? { ...selectedOrder, idTec: idNewTech, nomeTec: nomeTec } : selectedOrder;
     setSelectedOrder(updatedSelectedOrder);
-  
+
     await apiClient.post(`/maps/updateTec`, { idOS: editingOrder, idTec: idNewTech });
-    
+
     setEditingOrder(null);
   };
 
   return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={mapContainerStyle}
-      center={initialMapCenter}
-      zoom={12}
-      options={{
-        styles: [{ featureType: 'poi', stylers: [{ visibility: 'off' }] }],
-        mapTypeControl: false,
-      }}
-      onClick={handleMapClick}
-    >
-      {orders.map(order => (
-        <Marker
-          key={order.id}
-          position={{ lat: order.lat, lng: order.lng }}
-          icon={{ url: getMarkerIcon(highlightedOrder === order.id, order.nomeTec, tecList.find(tec => tec.id == order.idTec)) }}
-          onClick={handleMarkerClick(order)}
-          onMouseOut={handleOrderMouseOut}
-          onMouseOver={handleOrderMouseOver(order)}
-        />
-      ))}
-
-      {motos.map(moto => (
-        <Marker
-          key={moto.id}
-          position={{ lat: moto.lat, lng: moto.lng }}
-          icon={{ url: `/icon/motorcycling.png` }}
-          onMouseOut={handleMotoMouseOut}
-          onMouseOver={handleMotoMouseOver(moto)}
-        />
-      ))}
-
-      {(highlightedOrder || highlightedMoto) && (
-        <InfoWindow
-          position={{
-            lat: highlightedOrder ? (
-                orders.find(order => order.id === highlightedOrder).lat
-              ) : (
-                motos.find(moto => moto.id === highlightedMoto).lat
-              ),
-            lng: highlightedOrder ? (
-                orders.find(order => order.id === highlightedOrder).lng
-              ) : (
-                motos.find(moto => moto.id === highlightedMoto).lng
-              )
-            }}
-          options={{ pixelOffset: new window.google.maps.Size(0, -40), disableAutoPan: true }}
-        >
-          {
-            highlightedOrder ? (
-              <InfoWindowContentOrder order={orders.find(order => order.id === highlightedOrder)} />
-            ) : (
-              <InfoWindowContentMoto moto={motos.find(moto => moto.id === highlightedMoto)} />
-            )
-          }
-        </InfoWindow>
-      )}
-
-      {selectedOrder && (
-        <InfoWindow
-          position={{ lat: selectedOrder.lat, lng: selectedOrder.lng }}
-          options={{ pixelOffset: new window.google.maps.Size(0, -40), disableAutoPan: true }}
-        >
-          <InfoWindowContentOrder
-            key={selectedOrder.id + selectedOrder.nomeTec} // Unique key to force re-render
-            order={selectedOrder}
-            isEditing={editingOrder === selectedOrder.id}
-            onEditClick={() => handleEditTech(selectedOrder)}
-            onTecChange={handleTecChange}
-            tecList={tecList}
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        center={initialMapCenter}
+        zoom={12}
+        options={{
+          styles: [{ featureType: 'poi', stylers: [{ visibility: 'off' }] }],
+          mapTypeControl: false,
+        }}
+        onClick={handleMapClick}
+      >
+        {orders.map(order => (
+          <Marker
+            key={order.id}
+            position={{ lat: order.lat, lng: order.lng }}
+            icon={{ url: getMarkerIcon(highlightedOrder === order.id, order.nomeTec, tecList.find(tec => tec.id == order.idTec)) }}
+            onClick={handleMarkerClick(order)}
+            onMouseOut={handleOrderMouseOut}
+            onMouseOver={handleOrderMouseOver(order)}
           />
-        </InfoWindow>
-      )}
-    </GoogleMap>
+        ))}
+
+        {motos.map(moto => (
+          <Marker
+            key={moto.id}
+            position={{ lat: moto.lat, lng: moto.lng }}
+            icon={{ url: `/icon/motorcycling.png` }}
+            onMouseOut={handleMotoMouseOut}
+            onMouseOver={handleMotoMouseOver(moto)}
+          />
+        ))}
+
+        {(highlightedOrder || highlightedMoto) && (
+          <InfoWindow
+            position={{
+              lat: highlightedOrder ? (
+                  orders.find(order => order.id === highlightedOrder).lat
+                ) : (
+                  motos.find(moto => moto.id === highlightedMoto).lat
+                ),
+              lng: highlightedOrder ? (
+                  orders.find(order => order.id === highlightedOrder).lng
+                ) : (
+                  motos.find(moto => moto.id === highlightedMoto).lng
+                )
+              }}
+            options={{ pixelOffset: new window.google.maps.Size(0, -40), disableAutoPan: true }}
+          >
+            {
+              highlightedOrder ? (
+                <InfoWindowContentOrder order={orders.find(order => order.id === highlightedOrder)} />
+              ) : (
+                <InfoWindowContentMoto moto={motos.find(moto => moto.id === highlightedMoto)} />
+              )
+            }
+          </InfoWindow>
+        )}
+
+        {selectedOrder && (
+          <InfoWindow
+            position={{ lat: selectedOrder.lat, lng: selectedOrder.lng }}
+            options={{ pixelOffset: new window.google.maps.Size(0, -40), disableAutoPan: true }}
+          >
+            <InfoWindowContentOrder
+              key={selectedOrder.id + selectedOrder.nomeTec} // Unique key to force re-render
+              order={selectedOrder}
+              isEditing={editingOrder === selectedOrder.id}
+              onEditClick={() => handleEditTech(selectedOrder)}
+              onTecChange={handleTecChange}
+              tecList={tecList}
+            />
+          </InfoWindow>
+        )}
+      </GoogleMap>
   ) : null;
 };
 
